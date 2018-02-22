8b8383401093a0290be8cf2b5fea8cd9aa2c1628
Osiris <tim@chaubet.be>
2018-02-22 20:40:05 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,6 +6,7 @@ if [ -f /config/$(echo $initfile) ]; then
     echo 'initial configuration done.'
 else    
     git clone https://github.com/TrueOsiris/spotweb.git /www/spotweb
+    echo "<? header('Location: /spotweb/'); ?>" > /www/index.php
     echo -e "Do not remove this file.\nIf you do, container will be fully reset on next start." > /config/$(echo $initfile)
     date >> /config/$(echo $initfile)
 fi
