39b97caef868d82832ddb3116f76489458c95eae
Osiris <tim@chaubet.be>
2018-02-23 00:08:28 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -49,17 +49,17 @@ fi
 
 if [[ -n "$SPOTWEB_DB_TYPE" && -n "$SPOTWEB_DB_HOST" && -n "$SPOTWEB_DB_NAME" && -n "$SPOTWEB_DB_USER" && -n "$SPOTWEB_DB_PASS" ]]; then
 #    echo "Creating database configuration"
-    if [[ ! -s /config/dbsettings.inc.php ]]; then
+    if [[ -s /config/dbsettings.inc.php ]]; then
+       	echo "$(date +%S)"
+ 	if [ $((`date +%S` % 5)) = 0 ]; then
+		echo "test" 
+	fi
+    else
         echo "<?php" > /config/dbsettings.inc.php
         echo "\$dbsettings['engine'] = '$SPOTWEB_DB_TYPE';" >> /config/dbsettings.inc.php
         echo "\$dbsettings['host'] = '$SPOTWEB_DB_HOST';" >> /config/dbsettings.inc.php
         echo "\$dbsettings['dbname'] = '$SPOTWEB_DB_NAME';"  >> /config/dbsettings.inc.php
         echo "\$dbsettings['user'] = '$SPOTWEB_DB_USER';" >> /config/dbsettings.inc.php
         echo "\$dbsettings['pass'] = '$SPOTWEB_DB_PASS';"  >> /config/dbsettings.inc.php
-    else
-    	echo "$(date +%S)"
- 	if [ $((`date +%S` % 5)) = 0 ]; then
-		echo "test" 
-	fi
     fi
 fi
