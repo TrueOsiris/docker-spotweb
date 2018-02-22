f7b9a37e0c2bf2cb966dfe602b3cbd2c62cba6e9
Osiris <tim@chaubet.be>
2018-02-22 23:39:49 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,10 +12,14 @@ else
     echo "> Changing ownership of on /www/spotweb ..."
     chown -R www-data:www-data /www/spotweb
     echo "> Ownership changed."
+    echo "> Creating index.php in root folder ..."
     echo "<? header('Location: /spotweb/'); ?>" > /www/index.php
+    echo "> Creating /config/dbsettings.inc.php ..."
     touch /config/dbsettings.inc.php 2>/dev/null
+    echo "> Changing ownership of /config/dbsettings.inc.php ..."
     chown www-data:www-data /config/dbsettings.inc.php
-    rm /www/spotweb/dbsettings.inc.php 2>/dev/null
+    #rm /www/spotweb/dbsettings.inc.php 2>/dev/null
+    echo "> Creating symlink to /config/dbsettings.inc.php in /www/spotweb/ ..."
     ln -s /config/dbsettings.inc.php /www/spotweb/dbsettings.inc.php 2>/dev/null
     # creating file, so previous statements only run once.
     echo -e "Do not remove this file.\nIf you do, container will be fully reset on next start." > /config/$(echo $initfile)
@@ -23,7 +27,10 @@ else
 fi
 
 if [ ! -f /config/ownsettings.php ]; then
-    touch /config/ownsettings.php && chown www-data:www-data /config/ownsettings.php
+    echo "> Creating /config/ownsettings.php ..."
+    touch /config/ownsettings.php 
+    chown www-data:www-data /config/ownsettings.php
+    echo "> Creating symlink to /config/ownsettings.php in /www/spotweb/ ..."
     ln -s /config/ownsettings.php /www/spotweb/ownsettings.php
 fi
 
