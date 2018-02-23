5d1913dd0ed1919ff4da6d23aeabced64c214300
Osiris <tim@chaubet.be>
2018-02-23 21:07:35 +0100

    Update cycle.sh

--- cycle.sh
+++ cycle.sh
@@ -57,8 +57,8 @@ if [ $((`date +%M` % 15)) = 0 ]; then
   	# every 15 minutes  
 	echo "> Retrieval of new spots starting at $(date) ..."
 	if [[ -s $(echo $initfile) ]]; then
-	    echo "> Retrieving ..."
 	    /usr/bin/php /www/spotweb/retrieve.php --force >/config/retrieve.log 2>&1
+	    echo "> Retrieval finished at $(date)."
 	fi
     fi
 fi
