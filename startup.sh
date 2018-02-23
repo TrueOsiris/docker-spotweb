48fc98377d79e05c7842454a86ad2050483a5a64
Osiris <tim@chaubet.be>
2018-02-23 21:29:44 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,14 +13,14 @@ else
     chown -R www-data:www-data /www/spotweb
     echo "> Ownership changed."
     echo "> Creating index.php in root folder ..."
-    echo "<? header('Location: /spotweb/'); ?>" > /www/index.php
-    echo "> Creating /config/dbsettings.inc.php ..."
-    touch /config/dbsettings.inc.php 2>/dev/null
-    echo "> Changing ownership of /config/dbsettings.inc.php ..."
-    chown www-data:www-data /config/dbsettings.inc.php 2>&1
+    echo "<? header('Location: /spotweb/install.php'); ?>" > /www/index.php
+    echo "> Creating /config/dbsettings.inc.php.sample ..."
+    touch /config/dbsettings.inc.php.sample 2>/dev/null
+    echo "> Changing ownership of /config/dbsettings.inc.php.sample ..."
+    chown www-data:www-data /config/dbsettings.inc.php.sample 2>&1
     #rm /www/spotweb/dbsettings.inc.php 2>/dev/null
-    echo "> Creating symlink to /config/dbsettings.inc.php in /www/spotweb/ ..."
-    ln -s /config/dbsettings.inc.php /www/spotweb/dbsettings.inc.php 2>/dev/null
+    #echo "> Creating symlink to /config/dbsettings.inc.php in /www/spotweb/ ..."
+    #ln -s /config/dbsettings.inc.php /www/spotweb/dbsettings.inc.php 2>/dev/null
 
     # creating file, so previous statements only run once.
     echo -e "Do not remove this file.\nIf you do, container will be fully reset on next start." > /config/$(echo $initfile)
